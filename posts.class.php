bbe62091b75dc436c62919ea1094bdb4e5199009
Charl Dunois <hyoung45@icloud.com>
2014-01-01 17:02:50 +0000

    Update posts.class.php

--- posts.class.php
+++ posts.class.php
@@ -6,29 +6,30 @@ class Posts {
 	public $user_id = null;
 	public $posts = null;
 	public $clubs = array( 
-			 "<i class='icon-f-bread'></i> Roll Club - 500" => 500
-			,"<i class='icon-f-cake-plain'></i> Crumpet Club - 1000" => 1000
-			,"<i class='icon-f-telephone-handset'></i> BitesizeCookie Club - 2000" => 2000
-			,"<i class='icon-f-breads'></i> Crunch Club - 2600" => 2600
-			,"<i class='icon-f-magnifier-left'></i> MysteryScience Club - 3000" => 3000
-			,"<i class='icon-f-shoe'></i> LDR Club - 5000" => 5000
-			,"<i class='icon-f-computer'></i> IBMPC Club - 8088" => 8088
-			,"<i class='icon-f-cookie-bite'></i> Cookie Club - 10000" => 10000
-			,"<i class='icon-f-pipette'></i> SpinalTap Club - 11000" => 11000
-			,"<i class='icon-f-cup'></i> Breakfast Club - 20000" => 20000
-			,"<i class='icon-f-diamond'></i> Carat Club - 24000" => 24000
-			,"<i class='icon-f-compass'></i> Peshawar Club - 25000" => 25000
-			,"<i class='icon-f-paper-plane'></i> MileHigh Club - 30000" => 30000
-			,"<i class='icon-f-chart-pie-separate'></i> Pi Club - 31416" => 31416
-			,"<i class='icon-f-water'></i> Towel Club - 42000" => 42000
-			,"<i class='icon-f-hamburger'></i> Bacon Club - 50000" => 50000
-			,"<i class='icon-f-crown'></i> Commodore Club - 64000" => 64000
-			,"<i class='icon-f-globe'></i> Motorola Club - 68000" => 68000
-			,"<i class='icon-f-music-beam-16'></i> Trombone Club - 76000" => 76000
-			,"<i class='icon-f-network-clouds'></i> WiFi Club - 80211" => 80211
-			,"<i class='icon-f-language'></i> Tower Of Babble Club - 100000" => 100000
-			,"<i class='icon-f-mac-os'></i> Mac Club - 128000" => 128000
-			,"<i class='icon-f-balloon-twitter'></i> TwitterLeaver Club - 144000" => 144000
+			 "<i class='icon-f-bread'></i> - Roll Club - 500" => 500
+			,"<i class='icon-f-cake'></i> - Crumpet Club - 1000" => 1000
+			,"<i class='icon-f-telephone-handset'></i> - BitesizeCookie Club - 2000" => 2000
+			,"<i class='icon-f-breads'></i> - Crunch Club - 2600" => 2600
+			,"<i class='icon-f-magnifier-left'></i> - MysteryScience Club - 3000" => 3000
+			,"<i class='icon-f-shoe'></i> - LDR Club - 5000" => 5000
+			,"<i class='icon-f-television'></i> - IBMPC Club - 8088" => 8088
+			,"<i class='icon-f-cookie'></i> - Cookie Club - 10000" => 10000
+			,"<i class='icon-f-pipette'></i> - SpinalTap Club - 11000" => 11000
+			,"<i class='icon-f-cup'></i> - Breakfast Club - 20000" => 20000
+			,"<i class='icon-f-diamond'></i> - Carat Club - 24000" => 24000
+			,"<i class='icon-f-compass'></i> - Peshawar Club - 25000" => 25000
+			,"<i class='icon-f-paper-plane'></i> - MileHigh Club - 30000" => 30000
+			,"<i class='icon-f-chart-pie'></i> - Pi Club - 31416" => 31416
+			,"<i class='icon-f-water'></i> - Towel Club - 42000" => 42000
+			,"<i class='icon-f-piggy-bank-empty'></i> - Bacon Club - 50000" => 50000
+			,"<i class='icon-f-crown'></i> - Commodore Club - 64000" => 64000
+			,"<i class='icon-f-globe'></i> - Motorola Club - 68000" => 68000
+			,"<i class='icon-f-music-beam-16'></i> - Trombone Club - 76000" => 76000
+			,"<i class='icon-f-transmitter'></i> - WiFi Club - 80211" => 80211
+			,"<i class='icon-f-money'></i> - Pajama Club - 90000" => 90000
+			,"<i class='icon-f-language'></i> - Tower Of Babble Club - 100000" => 100000
+			,"<i class='icon-f-mac-os'></i> - Mac Club - 128000" => 128000
+			,"<i class='icon-f-balloon-twitter'></i> - TwitterLeaver Club - 144000" => 144000
 			,"<i class=''></i> GetALifeNoSrsly Club - 200000" => 200000
 			,"<i class=''></i> MeaninglessPostCount Club - 231568" => 231568
 			,"<i class=''></i> ADN Club - 256000" => 256000
