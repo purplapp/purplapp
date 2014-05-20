0c0df70b5a32b6ea3cf59cad38b893ab620e845f
Charlotte W <cwelkin@icloud.com>
2014-05-20 13:58:19 +0100

    Legal / support / nicer dates
    We made your dates nicer. Fancy.

--- ADN_php/newFunctions.php
+++ ADN_php/newFunctions.php
@@ -4,41 +4,41 @@ class PostClubs {
 	public $user_posts = null;
 
 	public $clubs = array(
-		array("name" => "#RollClub", "url" => "http://alpha.jvimedia.org/hashtags/RollClub", "count" => "500")
-		,array("name" => "#CrumpetClub", "url" => "http://alpha.jvimedia.org/hashtags/CrumpetClub", "count" => "1 000")
-		,array("name" => "#NoonClub", "url" => "http://alpha.jvimedia.org/hashtags/NoonClub", "count" => "1 200")
-		,array("name" => "#BitesizeCookieClub", "url" => "http://alpha.jvimedia.org/hashtags/BitesizeCookieClub", "count" => "2 000")
-		,array("name" => "#CrunchClub", "url" => "http://alpha.jvimedia.org/hashtags/CrunchClub", "count" => "2 600")
-		,array("name" => "#MysteryScienceClub", "url" => "http://alpha.jvimedia.org/hashtags/MysteryScienceClub", "count" => "3 000")
-		,array("name" => "#LDRClub", "url" => "http://alpha.jvimedia.org/hashtags/LDRClub", "count" => "5 000")
-		,array("name" => "#IBMPCClub", "url" => "http://alpha.jvimedia.org/hashtags/IBMPCClub", "count" => "8 088")
-		,array("name" => "#CookieClub", "url" => "http://alpha.jvimedia.org/hashtags/CookieClub", "count" => "10 000")
-		,array("name" => "#SpinalTapClub", "url" => "http://alpha.jvimedia.org/hashtags/SpinalTapClub", "count" => "11 000")
-		,array("name" => "#BreakfastClub", "url" => "http://alpha.jvimedia.org/hashtags/BreakfastClub", "count" => "20 000")
-		,array("name" => "#CaratClub", "url" => "http://alpha.jvimedia.org/hashtags/CaratClub", "count" => "24 000")
-		,array("name" => "#PeshawarClub", "url" => "http://alpha.jvimedia.org/hashtags/PeshawarClub", "count" => "25 000")
-		,array("name" => "#MileHighClub", "url" => "http://alpha.jvimedia.org/hashtags/MileHighClub", "count" => "30 000")
-		,array("name" => "#PiClub", "url" => "http://alpha.jvimedia.org/hashtags/PiClub", "count" => "31 416")
-		,array("name" => "#TowelClub", "url" => "http://alpha.jvimedia.org/hashtags/TowelClub", "count" => "42 000")
-		,array("name" => "#HitmanClub", "url" => "http://alpha.jvimedia.org/hashtags/HitmanClub", "count" => "47 000")
-		,array("name" => "#BaconClub", "url" => "http://alpha.jvimedia.org/hashtags/BaconClub", "count" => "50 000")
-		,array("name" => "#BrowncoatClubClub", "url" => "http://alpha.jvimedia.org/hashtags/BrowncoatClubClub", "count" => "57 000")
-		,array("name" => "#CommodoreClub", "url" => "http://alpha.jvimedia.org/hashtags/CommodoreClub", "count" => "64 000")
-		,array("name" => "#MotorolaClub", "url" => "http://alpha.jvimedia.org/hashtags/MotorolaClub", "count" => "68 000")
-		,array("name" => "#TromboneClub", "url" => "http://alpha.jvimedia.org/hashtags/TromboneClub", "count" => "76 000")
-		,array("name" => "#WiFiClub", "url" => "http://alpha.jvimedia.org/hashtags/WiFiClub", "count" => "80 211")
-		,array("name" => "#PajamaClub", "url" => "http://alpha.jvimedia.org/hashtags/PajamaClub", "count" => "90 000")
-		,array("name" => "#TowerOfBabbleClub", "url" => "http://alpha.jvimedia.org/hashtags/TowerOfBabbleClub", "count" => "100 000")
-		,array("name" => "#MacClub", "url" => "http://alpha.jvimedia.org/hashtags/MacClub", "count" => "128 000")
-		,array("name" => "#TwitterLeaverClub", "url" => "http://alpha.jvimedia.org/hashtags/TwitterLeaverClub", "count" => "140 000")
-		,array("name" => "#GetALifeNoSrslyClub", "url" => "http://alpha.jvimedia.org/hashtags/GetALifeNoSrslyClub", "count" => "200 000")
-		,array("name" => "#MeaninglessPostCountClub", "url" => "http://alpha.jvimedia.org/hashtags/MeaninglessPostCountClub", "count" => "231 568")
-		,array("name" => "#ADNClub", "url" => "http://alpha.jvimedia.org/hashtags/ADNClub", "count" => "256 000")
-		,array("name" => "#PensionersClub", "url" => "http://alpha.jvimedia.org/hashtags/PensionersClub", "count" => "401 000")
-		,array("name" => "#LaughterClub", "url" => "http://alpha.jvimedia.org/hashtags/LaughterClub", "count" => "555 000")
-		,array("name" => "#GatesClub", "url" => "http://alpha.jvimedia.org/hashtags/GatesClub", "count" => "640 000")
-		,array("name" => "#JoyLuckClub", "url" => "http://alpha.jvimedia.org/hashtags/JoyLuckClub", "count" => "888 000")
-		,array("name" => "#MillionairesClub", "url" => "http://alpha.jvimedia.org/hashtags/MillionairesClub", "count" => "1 000 000")
+		array("name" => "#RollClub", "url" => "RollClub", "count" => "500")
+		,array("name" => "#CrumpetClub", "url" => "CrumpetClub", "count" => "1 000")
+		,array("name" => "#NoonClub", "url" => "NoonClub", "count" => "1 200")
+		,array("name" => "#BitesizeCookieClub", "url" => "BitesizeCookieClub", "count" => "2 000")
+		,array("name" => "#CrunchClub", "url" => "CrunchClub", "count" => "2 600")
+		,array("name" => "#MysteryScienceClub", "url" => "MysteryScienceClub", "count" => "3 000")
+		,array("name" => "#LDRClub", "url" => "LDRClub", "count" => "5 000")
+		,array("name" => "#IBMPCClub", "url" => "IBMPCClub", "count" => "8 088")
+		,array("name" => "#CookieClub", "url" => "CookieClub", "count" => "10 000")
+		,array("name" => "#SpinalTapClub", "url" => "SpinalTapClub", "count" => "11 000")
+		,array("name" => "#BreakfastClub", "url" => "BreakfastClub", "count" => "20 000")
+		,array("name" => "#CaratClub", "url" => "CaratClub", "count" => "24 000")
+		,array("name" => "#PeshawarClub", "url" => "PeshawarClub", "count" => "25 000")
+		,array("name" => "#MileHighClub", "url" => "MileHighClub", "count" => "30 000")
+		,array("name" => "#PiClub", "url" => "PiClub", "count" => "31 416")
+		,array("name" => "#TowelClub", "url" => "TowelClub", "count" => "42 000")
+		,array("name" => "#HitmanClub", "url" => "HitmanClub", "count" => "47 000")
+		,array("name" => "#BaconClub", "url" => "BaconClub", "count" => "50 000")
+		,array("name" => "#BrowncoatClubClub", "url" => "BrowncoatClubClub", "count" => "57 000")
+		,array("name" => "#CommodoreClub", "url" => "CommodoreClub", "count" => "64 000")
+		,array("name" => "#MotorolaClub", "url" => "MotorolaClub", "count" => "68 000")
+		,array("name" => "#TromboneClub", "url" => "TromboneClub", "count" => "76 000")
+		,array("name" => "#WiFiClub", "url" => "WiFiClub", "count" => "80 211")
+		,array("name" => "#PajamaClub", "url" => "PajamaClub", "count" => "90 000")
+		,array("name" => "#TowerOfBabbleClub", "url" => "TowerOfBabbleClub", "count" => "100 000")
+		,array("name" => "#MacClub", "url" => "MacClub", "count" => "128 000")
+		,array("name" => "#TwitterLeaverClub", "url" => "TwitterLeaverClub", "count" => "140 000")
+		,array("name" => "#GetALifeNoSrslyClub", "url" => "GetALifeNoSrslyClub", "count" => "200 000")
+		,array("name" => "#MeaninglessPostCountClub", "url" => "MeaninglessPostCountClub", "count" => "231 568")
+		,array("name" => "#ADNClub", "url" => "ADNClub", "count" => "256 000")
+		,array("name" => "#PensionersClub", "url" => "PensionersClub", "count" => "401 000")
+		,array("name" => "#LaughterClub", "url" => "LaughterClub", "count" => "555 000")
+		,array("name" => "#GatesClub", "url" => "GatesClub", "count" => "640 000")
+		,array("name" => "#JoyLuckClub", "url" => "JoyLuckClub", "count" => "888 000")
+		,array("name" => "#MillionairesClub", "url" => "MillionairesClub", "count" => "1 000 000")
 	); 
 
 	public $memberclubs = array();
@@ -46,13 +46,82 @@ class PostClubs {
 	public function setUserPost($get) {
 		$this->user_posts = $get;
 	}
+	
+	public function setAlpha($get) {
+		$this->alpha = $get;
+	}
 
 	public function getClubs() {
 		foreach($this->clubs as $club) {
 			if($this->user_posts > preg_replace("/[^0-9,.]/", "", $club['count'])) {
-				$this->memberclubs[] = "{$club['name']} - <a href='{$club['url']}'>on Alpha</a> - {$club['count']}";
+				$this->memberclubs[] = "<a href='{$this->alpha}hashtags/{$club['url']}' target='_blank'>{$club['name']}</a> ({$club['count']} posts)";
 			}
 		}
 	}
+}
+
+class PostData {
+
+	/** 
+		* A sweet interval formatting, will use the two biggest interval parts. 
+		* On small intervals, you get minutes and seconds. 
+		* On big intervals, you get months and days. 
+		* Only the two biggest parts are used. 
+		* 
+		* @param DateTime $start 
+		* @param DateTime|null $end 
+		* @return string 
+	*/ 
+	
+	public function formatDateDiff($start, $end=null) { 
+	    if(!($start instanceof DateTime)) { 
+	        $start = new DateTime($start); 
+	    } 
+	    
+	    if($end === null) { 
+	        $end = new DateTime(); 
+	    } 
+	    
+	    if(!($end instanceof DateTime)) { 
+	        $end = new DateTime($start); 
+	    } 
+	    
+	    $interval = $end->diff($start); 
+	    $doPlural = function($nb,$str){return $nb>1?$str.'s':$str;}; // adds plurals 
+	    
+	    $format = array(); 
+	    if($interval->y !== 0) { 
+	        $format[] = "%y ".$doPlural($interval->y, "year"); 
+	    } 
+	    if($interval->m !== 0) { 
+	        $format[] = "%m ".$doPlural($interval->m, "month"); 
+	    } 
+	    if($interval->d !== 0) { 
+	        $format[] = "%d ".$doPlural($interval->d, "day"); 
+	    } 
+	    if($interval->h !== 0) { 
+	        $format[] = "%h ".$doPlural($interval->h, "hour"); 
+	    } 
+	    if($interval->i !== 0) { 
+	        $format[] = "%i ".$doPlural($interval->i, "minute"); 
+	    } 
+	    if($interval->s !== 0) { 
+	        if(!count($format)) { 
+	            return "less than a minute"; 
+	        } else { 
+	            $format[] = "%s ".$doPlural($interval->s, "second"); 
+	        } 
+	    } 
+	    
+	    // We use the two biggest parts 
+	    if(count($format) > 1) { 
+	        $format = array_shift($format)." and ".array_shift($format); 
+	    } else { 
+	        $format = array_pop($format); 
+	    } 
+	    
+	    // Prepend 'since ' or whatever you like 
+	    return $interval->format($format); 
+	} 
 }
 ?>
--- account/user.php
+++ account/user.php
@@ -1,15 +1,20 @@
 <?php
-    // error_reporting(E_ALL);
-    // ini_set("display_errors", 1);  
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);  
 
     require_once '../ADN_php/EZAppDotNet.php';
     require('../ADN_php/newFunctions.php');
-
+    require('../ADN_php/nicerank.php');
+        
     $app = new EZAppDotNet();
 
     $user_params = array(
         'include_user_annotations' => true, 
     );
+    
+    //calculate current date for day calc
+    $today = date('Y-m-d H:i:s');
+    $start = new DateTime($today);
 
     // check that the user is signed in
     if ($app->getSession()) {
@@ -44,13 +49,35 @@
             $data = $app->getUser($user_number, $user_params);
 
         }
+        
+        //calculate date created for day calc
+        $createdat= new DateTime($data['created_at']);
+        $adnjoin = $createdat->format('Y-m-d H:i:s');
 
+        //calculate number of days on ADN
+        $date1 = new DateTime($adnjoin);
+        $date2 = new DateTime($today);
+        $interval = $date1->diff($date2);
+		
+		// pca club functions
         $clubs = new PostClubs;
-
+		
+		$clubs->setAlpha($alpha);
         $clubs->setUserPost($data['counts']['posts']);
         $clubs->getClubs();
 
-        $user_clubs = $clubs->memberclubs
+        $user_clubs = $clubs->memberclubs;
+        
+		// post-date functions
+		$posts = new PostData;
+		
+		// nicerank
+		$nicerank = new NiceRank;
+		
+		$nicerank->setUserID($user_number);
+		$nicerank->getNiceRank();
+		
+		$nice_rank_data = $nicerank->nicerank;
 ?>
 
 <div class="col-md-12">
@@ -60,7 +87,7 @@
         <h1>
           <?php echo $data['name'] ?>
           <small>
-            <?php echo "<a class='url' href='http://alpha.jvimedia.org/".$data['username']."'>@".$data['username']."</a>" ?>
+            <?php echo "<a href='".$alpha, $data['username']."' target='_blank'>@".$data['username']."</a>" ?>
           </small>
         </h1>
     </div>
@@ -99,27 +126,49 @@
 
 <div class="col-md-6">
     <!--Info-->
-    <table class="table">
+    <table class="table table-condensed">
         <tr>
             <td><h4>ADN Data</h4></td>
             <td></td>
         </tr>
         <tr>
             <td>Posts:</td>
-            <td><?php echo $data['counts']['posts']; ?></td>
+            <td><?php echo "<a class='url' href='".$alpha, $data['username']."/posts/' target='_blank'>".$data['counts']['posts']."</a> <i>(average ".round(($data['counts']['posts'] / $interval->days), 2)." per day)</i>"; ?></td>
         </tr>
         <tr>
             <td>Starred:</td>
-            <td><?php echo $data['counts']['stars']; ?></td>
+            <td><?php echo "<a class='url' href='".$alpha, $data['username']."/stars/' target='_blank'>".$data['counts']['stars']."</a> <i>(average ".round(($data['counts']['stars'] / $interval->days), 2)." per day)</i>"; ?></td>
         </tr>
+        <?php if ($username != $auth_username) { ?>
         <tr>
             <td>Following:</td>
-            <td><?php echo $data['counts']['following']; ?></td>
+            <td>
+            	<?php
+            		if ($data['follows_you']){ $follows_you = "follows you"; } else { $follows_you = "does not follow you"; }
+            		echo "<a class='url' href='".$alpha, $data['username']."/following/' target='_blank'>".$data['counts']['following']."</a> <i>(".$follows_you.")</i>" 
+            	?>
+            </td>
         </tr>
         <tr>
             <td>Followers:</td>
-            <td><?php echo $data['counts']['followers']; ?></td>
+            <td>
+            	<?php
+            		if ($data['you_follow']){ $you_follow = "you follow"; } else { $you_follow = "you don't follow"; }
+            		echo "<a class='url' href='".$alpha, $data['username']."/followers/' target='_blank'>".$data['counts']['followers']."</a> <i>(".$you_follow.")</i>" 
+            	?>
+            </td>
         </tr>
+
+		<?php } else { ?>
+        <tr>
+            <td>Following:</td>
+            <td><?php echo "<a class='url' href='".$alpha, $data['username']."/following/' target='_blank'>".$data['counts']['following']."</a>" ?></td>
+        </tr>
+        <tr>
+            <td>Followers:</td>
+            <td><?php echo "<a class='url' href='".$alpha, $data['username']."/followers/' target='_blank'>".$data['counts']['followers']."</a>" ?></td>
+        </tr>
+        <?php } ?>
         <tr>
             <td>Account Type:</td>
             <td><?php echo ucfirst($data['type']); ?></td>
@@ -127,6 +176,10 @@
         <tr>
             <td>Location:</td>
             <td><?php echo $data['timezone']; ?></td>
+        </tr>
+        <tr>
+            <td>Locale:</td>
+            <td><?php echo $data['locale']; ?></td>
         </tr>
         <tr>
             <td>User Number:</td>
@@ -137,34 +190,14 @@
             <td>
                 <?php
                     $date = new DateTime($data['created_at']);
-                    $dateresult = $date->format('H:i:s Y-m-d');
-                    echo $dateresult;
-                ?>
-            </td>
-        </tr> 
-        <tr>
-            <td>ADN Age:</td>
-            <td>
-                <?php 
-                    //calculate current date for day calc
-                    $today = date('Y-m-d');
-
-                    //calculate date created for day calc
-                    $createdat= new DateTime($data['created_at']);
-                    $adnjoin = $createdat->format('Y-m-d');
-
-                    //calculate number of days on ADN
-                    $date1 = new DateTime($adnjoin);
-                    $date2 = new DateTime($today);
-                    $interval = $date1->diff($date2);
+                    $dateresult = $date->format('H:i \o\n d M Y');
                     
-                    echo $interval->days, " days";
+                    $end = new DateTime($adnjoin);
+			        $adnage = $posts->formatDateDiff($start, $end);
+					
+					echo $dateresult, " <i>(", $adnage, " ago)</i>";
                 ?>
             </td>
-        </tr>
-        <tr>
-            <td>Locale:</td>
-            <td><?php echo $data['locale']; ?></td>
         </tr>
         <?php 
             if(isset($data['verified_link'])) {
@@ -172,7 +205,8 @@
             } else {
                 $verified_link = "n";
             }
-        if (($verified_link = "y") and $data['annotations']) { ?>
+	        if (($verified_link = "y") and $data['annotations']) { 
+        ?>
         <tr>
             <td><h4>User Data</h4></td>
             <td></td>
@@ -183,7 +217,7 @@
                 $verified_link = $data['verified_link'];
                 echo "<td>Verified Domain:</td>";
                 echo "<td>";
-                echo "<a class='url' href='".$verified_link."'>".$data['verified_domain']."</a>";
+                echo "<a href='".$verified_link."' target='_blank'>".$data['verified_domain']."</a>";
                 echo "</td>";
             }
             ?>
@@ -195,7 +229,7 @@
                     $blogurl=$annotations['value']['url'];
                     echo "<td>Blog:</td>";
                     echo "<td>";
-                    echo "<a href=".$blogurl.">".parse_url($blogurl, PHP_URL_HOST)."</a>";
+                    echo "<a href=".$blogurl." target='_blank'>".parse_url($blogurl, PHP_URL_HOST)."</a>";
                     echo "</td>"; 
                 }
             }
@@ -206,9 +240,9 @@
             foreach($data['annotations'] as $annotations){
                 if (strpos($annotations['type'],"core.directory.facebook") == true){
                     $facebook_id=$annotations['value']['id'];
-                    echo "<td>Facebook ID:</td>";
+                    echo "<td>Facebook:</td>";
                     echo "<td>";
-                    echo '<a href="http://facebook.com/'.$facebook_id.'">'.$facebook_id.'</a>';
+                    echo '<a href="http://facebook.com/'.$facebook_id.'" target="_blank">'.$facebook_id.'</a>';
                     echo "</td>"; 
                 }
             }
@@ -219,9 +253,9 @@
             foreach($data['annotations'] as $annotations){
                 if (strpos($annotations['type'],"core.directory.twitter") == true){
                     $twitter_id=$annotations['value']['username'];
-                    echo "<td>Twitter ID:</td>";
+                    echo "<td>Twitter:</td>";
                     echo "<td>";
-                    echo '<a href="http://twitter.com/'.$twitter_id.'">@'.$twitter_id.'</a>';
+                    echo '<a href="http://twitter.com/'.$twitter_id.'" target="_blank">@'.$twitter_id.'</a>';
                     echo "</td>"; 
                 }
             }
@@ -258,20 +292,11 @@
 </div>
 
 <div class="col-md-6">
-    <table class="table">
+    <table class="table table-condensed">
         <?php if ($username != $auth_username) { ?>
         <tr>
             <td><h4>Comparison</h4></td>
             <td></td>
-        </tr>
-        <tr>
-            <td class="">Follows You:</td>
-            <td><?php if ($data['follows_you']){ echo "Yes"; } else { echo "No"; } ?></td>            
-        </tr>
-        <tr>
-            <td class="">You Follow:</td>
-            <td><?php if ($data['you_follow']){ echo "Yes"; } else { echo "No"; } ?></td>            
-        </tr>
         <tr>
             <td class="">Muted:</td>
             <td><?php if ($data['you_muted']){ echo "Yes"; } else { echo "No"; } ?></td>
@@ -297,11 +322,12 @@
                     $firstpost = $app->getUserPosts($user_id="$user_number", $post_params);
 
                     $created_at = new DateTime($firstpost[0]['created_at']);
-                    $firstpost_created_at = $created_at->format('Y-m-d H:i:s');
+                    $firstpost_created_at = $created_at->format('H:i \o\n d M Y');
 
-                    $firstpostlink = $firstpost[0]['canonical_url'];
+                    $firstpost_post_id = $firstpost[0]['id'];
+                    $firstpost_user = $firstpost[0]['user']['username'];
                 ?>
-                <a href="<?php echo $firstpostlink; ?>"><?php echo $firstpost_created_at; ?></a>
+                <a href="<?php echo $alpha, $firstpost_user, "/post/", $firstpost_post_id; ?>" target="_blank"><?php echo $firstpost_created_at; ?></a>
             </td>
         </tr>
 
@@ -314,17 +340,19 @@
 
             if ($firstmention) {
                 $created_at = new DateTime($firstmention[0]['created_at']);
-                $firstmention_created_at = $created_at->format('Y-m-d H:i:s');
+                $firstmention_created_at = $created_at->format('H:i \o\n d M Y');
 
                 $firstmentionlink = $firstmention[0]['canonical_url'];
 
                 $firstmention_user = $firstmention[0]['user']['username'];
                 $firstmention_user_link = $firstmention[0]['user']['canonical_url'];
+                
+                $firstmention_post_id = $firstmention[0]['id'];               
         ?>
         <tr>
             <td class="">First Mention:</td>
             <td>
-                <a href="<?php echo $firstmentionlink; ?>"><?php echo $firstmention_created_at; ?></a> by <a href="http://alpha.jvimedia.org/<?php echo $firstmention_user; ?>">@<?php echo $firstmention_user; ?></a>
+                <a href="<?php echo $alpha, $firstmention_user, "/post/", $firstmention_post_id; ?>" target='_blank'><?php echo $firstmention_created_at; ?></a> <i>(by <a href="http://alpha.jvimedia.org/<?php echo $firstmention_user; ?>" target='_blank'>@<?php echo $firstmention_user; ?>)</i></a>
             </td>
         </tr>               
         <?php } ?>
@@ -343,8 +371,14 @@
                     $lastpost_created_at = $created_at->format('Y-m-d H:i:s');
 
                     $lastpostlink = $lastpost[0]['canonical_url'];
+                    
+                    $end = new DateTime($lastpost_created_at);
+			        $lastpost_ago = $posts->formatDateDiff($start, $end);	
+
+			        $lastpost_post_id = $lastpost[0]['id'];
+                    $lastpost_user = $lastpost[0]['user']['username'];	 			
                 ?>
-                <a href="<?php echo $lastpostlink; ?>"><?php echo $lastpost_created_at; ?></a>
+                <a href="<?php echo $alpha, $lastpost_user, "/post/", $lastpost_post_id; ?>" target='_blank'><?php echo $lastpost_ago; ?> ago</a>
             </td>
         </tr>
 
@@ -363,25 +397,77 @@
 
                 $lastmention_user = $lastmention[0]['user']['username'];
                 $lastmention_user_link = $lastmention[0]['user']['canonical_url'];
+                
+                $end = new DateTime($lastmention_created_at);
+		        $lastmention_ago = $posts->formatDateDiff($start, $end);
+
+                $lastmention_post_id = $lastmention[0]['id'];               		        
         ?>
         <tr>
             <td class="">Last Mention:</td>
             <td>
-                <a href="<?php echo $lastmentionlink; ?>"><?php echo $lastmention_created_at; ?></a> by <a href="http://alpha.jvimedia.org/<?php echo $lastmention_user; ?>">@<?php echo $lastmention_user; ?></a>
+                <a href="<?php echo $alpha, $lastmention_user, "/post/", $lastmention_post_id; ?>" target='_blank'><?php echo $lastmention_ago; ?> ago</a> <i>(by <a href="http://alpha.jvimedia.org/<?php echo $lastmention_user; ?>" target='_blank'>@<?php echo $lastmention_user; ?>)</i></a>
             </td>
-        </tr>               
+        </tr>           
         <?php } ?>
-
+         
+        <?php } ?>
+        
+        <?php if (isset($nice_rank_data[0])) { ?>
         <tr>
-            <td>Posts Per Day:</td>
-            <td><?php echo round(($data['counts']['posts'] / $interval->days), 2); ?> (average)</td>    
+            <td><h4>NiceRank</h4></td>
+            <td></td>
+        </tr>        
+        <tr>
+        	<td>Rank:</td>
+        	<td><?php echo $nice_rank_data[0]->rank; ?></td>
         </tr>
         <tr>
-            <td>Stars Per Day:</td>
-            <td><?php echo round(($data['counts']['stars'] / $interval->days), 2); ?> (average)</td>    
-        </tr>            
+        	<td>Real Person:</td>
+        	<td>
+        		<?php 
+        			$real_person = $nice_rank_data[0]->account->real_person; 
+	        		
+	        		if ($real_person = '1') {
+		        		echo "Yes";
+	        		} else {
+		        		echo "No";
+	        		}
+        		?>
+        	</td>
+        </tr>
+        <tr>
+        	<td><h5>Past 28 days:</h5></td>
+			<td></td>
+        </tr>
+        <tr>
+        	<td>Robot_Posts:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->robo_posts; ?></td>
+        </tr>
+        <tr>
+        	<td>Posts:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->post_count; ?></td>
+        </tr>
+        <tr>
+        	<td>Conversations:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->conversations; ?></td>
+        </tr>
+        <tr>
+        	<td>Links:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->links; ?></td>
+        </tr>
+        <tr>
+        	<td>Mentions:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->mentions; ?></td>
+        </tr>
+        <tr>
+        	<td>Questions:</td>
+        	<td><?php echo $nice_rank_data[0]->stats->questions; ?></td>
+        </tr>
         <?php } ?>
-        <?php if ($username != $auth_username) { ?>     
+        
+        <!--
+<?php if ($username != $auth_username) { ?>     
         <tr>
             <td><h4>Spam User Check</h4></td>
             <td></td>
@@ -422,6 +508,8 @@
         </tr>
         <?php } ?>
         <?php } ?>
+-->
+        
         <?php if ($clubs->memberclubs == true) { ?>
         <tr>
             <td><h4>User PCA Clubs</h4></td>
@@ -446,7 +534,7 @@
             </td>
         </tr>
         <tr>
-            <td><a href='http://appdotnetwiki.net/w/index.php?title=Post_Count_Achievements'>More info on PCA clubs</a></td>
+            <td><a href='http://appdotnetwiki.net/w/index.php?title=Post_Count_Achievements' target='_blank'>More info on PCA clubs</a></td>
             <td></td>
         </tr>
         <?php } ?>
