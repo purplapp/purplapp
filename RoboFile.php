fc43ae2de0f8d99f9f3a271ba6a12fc342e4366d
Ciaran Downey <me@ciarand.me>
2014-09-19 21:02:31 -0700

    Add robo dependency, RoboFile

---
+++ RoboFile.php
@@ -0,0 +1,59 @@
+<?php require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Finder\Finder;
+use \Robo\Tasks as TaskList;
+use \Robo\Task\GenMarkdownDocTask as Doc;
+
+class RoboFile extends TaskList
+{
+    const SERVER_PORT = 8000;
+
+    /**
+     * @desc Start the built-in PHP web server
+     */
+    public function serve()
+    {
+        $this->say("Starting the built-in server on localhost:" . self::SERVER_PORT);
+
+        $this->getServer()->run();
+    }
+
+    /**
+     * @desc Runs the test suite
+     */
+    public function test($args = "")
+    {
+        $this->getServer()->background()->run();
+
+        $this->taskCodecept('./bin/codecept')
+            ->args($args)
+            ->run();
+    }
+
+    /**
+     * @desc watches the directory and reruns the tests when something is 
+     * changed
+     */
+    public function tdd()
+    {
+        $self  = $this;
+        $files = __DIR__ . "/tests/";
+
+        $this->taskWatch()
+            ->monitor($files, function ($event) use ($self) {
+                $path                 = (string) $event->getResource()->getResource();
+                $extension            = pathinfo($path, PATHINFO_EXTENSION);
+                $acceptableExtensions = array("php", "twig", "json");
+
+                if (in_array($extension, $acceptableExtensions)) {
+                    $self->test();
+                }
+            })
+            ->run();
+    }
+
+    private function getServer()
+    {
+        return $this->taskServer(self::SERVER_PORT)->dir(__DIR__);
+    }
+}
