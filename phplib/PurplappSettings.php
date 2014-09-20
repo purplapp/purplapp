44ca48d216ed531fd010bcdf511542c62fe87aa4
Charlotte W. <charlw@users.noreply.github.com>
2014-09-20 10:28:09 +0100

    Update PurplappSettings.php

--- phplib/PurplappSettings.php
+++ phplib/PurplappSettings.php
@@ -13,9 +13,9 @@ $app_clientId     = getenv("CLIENT_ID");
 $app_clientSecret = getenv("CLIENT_SECRET");
 
 // optional
-$alpha   = getenv("ALPHA_DOMAIN")  ?: "https://alpha.app.net/";
-$support = getenv("SUPPORT_EMAIL") ?: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$github  = getenv("GITHUB_URL")    ?: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$alpha   = getenv("ALPHA_DOMAIN")  ?: "https://alpha.app.net/";             // stick with this unless you have an alternative Alpha install
+$support = getenv("SUPPORT_EMAIL") ?: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";   // add in your support email here
+$github  = getenv("GITHUB_URL")    ?: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";   // if your project is on Github, add that link in here
 
 $app_redirectUri  = "http://{$_SERVER["HTTP_HOST"]}/phplib/PurplappCallback.php";
 
