4bd9e61404b6bc088b29931ed7453ba8c0c7cf66
Ciaran Downey <me@ciarand.me>
2014-09-28 16:14:13 -0700

    Mostly make the account/user page work

--- start/routes.php
+++ start/routes.php
@@ -16,8 +16,8 @@ $renderer = function ($name, array $data = []) use ($app) {
 };
 
 /**
- * generates a simple controller that renders templates for GET requests, binds 
- * it to the provided name, and sets up a redirect for legacy URLs that use the 
+ * generates a simple controller that renders templates for GET requests, binds
+ * it to the provided name, and sets up a redirect for legacy URLs that use the
  * filename.php convention
  *
  * @param string $path
@@ -53,7 +53,7 @@ $app->get("/signout", function () use ($app) {
     $app["session"]->invalidate();
 
     return $app->redirect($app->path("index"));
-});
+})->bind("signout");
 
 $app->get("/signin.php", $redirector("signin"));
 
@@ -77,4 +77,44 @@ $app->get("/account/mention", function (Request $req) use ($app) {
 
     $left  = $req->get("id1", "me");
     $right = $req->get("id2", "me");
-});
+})->bind("account_mention");
+
+$app->get("/account/user/{username}", function () use ($app) {
+    if (!$app["adn.user"]) {
+        return $app->render("unauth_message.twig");
+    }
+
+    $client = $app["adn.client"];
+
+    $user = $client->getAuthorizedUser(["include_user_annotations" => true]);
+
+    $firstPost = $client->getAuthorizedUserPosts(
+        ["count" => -1, "include_deleted" => false]
+    )->head();
+    $lastPost = $client->getAuthorizedUserPosts(
+        ["count" => 1, "include_deleted" => false]
+    )->tail();
+
+    $firstMention = $client->getAuthorizedUserMentions(
+        ["count" => -1, "include_deleted" => false]
+    )->head();
+    $lastMention = $client->getAuthorizedUserMentions(
+        ["count" => -1, "include_deleted" => false]
+    )->tail();
+
+    $niceRank = $client->getAuthorizedUserNiceRank();
+
+    return $app->render("account_user.twig", [
+        "user"          => $user,
+        "first_post"    => $firstPost,
+        "last_post"     => $lastPost,
+        "first_mention" => $firstMention,
+        "last_mention"  => $lastMention,
+        "niceRank"      => $niceRank,
+    ]);
+
+})->bind("account_user")->value("username", "me");
+
+$app->get("/account/follow_comparison", function () use ($app) {
+    return $app->render("account_follow_comparison.twig");
+})->bind("account_follow_comparison");
