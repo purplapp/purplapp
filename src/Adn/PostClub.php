c270e8f774bc9c3414f4cf8cda8c0a27ed060072
Ciaran Downey <me@ciarand.me>
2014-10-04 11:10:56 -0700

    Update PostClub(s) classes with cleaner logic
    Also add OrphanBlackClub back. Yay?

--- src/Adn/PostClub.php
+++ src/Adn/PostClub.php
@@ -6,7 +6,17 @@ class PostClub
 
     public function html()
     {
-        return "<a href=\"https://alpha.app.net/hashtags/{$this->url}\">{$this->name}</a>"
-            . "<i>({$this->nicecount} posts)</i>";
+        return "<a href=\"https://alpha.app.net/hashtags/{$this->url}\">{$this->name()}</a>"
+            . "<i>({$this->niceCount()} posts)</i>";
+    }
+
+    public function niceCount()
+    {
+        return number_format($this->count, 0, ".", " ");
+    }
+
+    public function name()
+    {
+        return "#{$this->url}";
     }
 }
--- src/Adn/PostClubs.php
+++ src/Adn/PostClubs.php
@@ -4,222 +4,152 @@ class PostClubs
 {
     private static $clubs = [
         [
-            "name"      => "#RollClub",
             "url"       => "RollClub",
-            "nicecount" => "500",
             "count"     => "500"
         ],
         [
-            "name"      => "#CrumpetClub",
             "url"       => "CrumpetClub",
-            "nicecount" => "1 000",
             "count"     => "1000"
         ],
         [
-            "name"      => "#NoonClub",
             "url"       => "NoonClub",
-            "nicecount" => "1 200",
             "count"     => "1200"
         ],
         [
-            "name"      => "#BitesizeCookieClub",
             "url"       => "BitesizeCookieClub",
-            "nicecount" => "2 000",
             "count"     => "2000"
         ],
         [
-            "name"      => "#CrunchClub",
             "url"       => "CrunchClub",
-            "nicecount" => "2 600",
             "count"     => "2600"
         ],
         [
-            "name"      => "#MysteryScienceClub",
             "url"       => "MysteryScienceClub",
-            "nicecount" => "3 000",
             "count"     => "3000"
         ],
         [
-            "name"      => "#LDRClub",
             "url"       => "LDRClub",
-            "nicecount" => "5 000",
             "count"     => "5000"
         ],
         [
-            "name"      => "#IBMPCClub",
             "url"       => "IBMPCClub",
-            "nicecount" => "8 088",
             "count"     => "8088"
         ],
         [
-            "name"      => "#CookieClub",
             "url"       => "CookieClub",
-            "nicecount" => "10 000",
             "count"     => "10000"
         ],
         [
-            "name"      => "#SpinalTapClub",
             "url"       => "SpinalTapClub",
-            "nicecount" => "11 000",
             "count"     => "11000"
         ],
         [
-            "name"      => "#BreakfastClub",
             "url"       => "BreakfastClub",
-            "nicecount" => "20 000",
             "count"     => "20000"
         ],
         [
-            "name"      => "#CaratClub",
             "url"       => "CaratClub",
-            "nicecount" => "24 000",
             "count"     => "24000"
         ],
         [
-            "name"      => "#PeshawarClub",
             "url"       => "PeshawarClub",
-            "nicecount" => "25 000",
             "count"     => "25000"
         ],
         [
-            "name"      => "#MileHighClub",
             "url"       => "MileHighClub",
-            "nicecount" => "30 000",
             "count"     => "30000"
         ],
         [
-            "name"      => "#PiClub",
             "url"       => "PiClub",
-            "nicecount" => "31 416",
             "count"     => "31416"
         ],
         [
-            "name"      => "#TowelClub",
             "url"       => "TowelClub",
-            "nicecount" => "42 000",
             "count"     => "42000"
         ],
         [
-            "name"      => "#HitmanClub",
             "url"       => "HitmanClub",
-            "nicecount" => "47 000",
             "count"     => "47000"
         ],
         [
-            "name"      => "#BaconClub",
             "url"       => "BaconClub",
-            "nicecount" => "50 000",
             "count"     => "50000"
         ],
         [
-            "name"      => "#BrowncoatClub",
             "url"       => "BrowncoatClub",
-            "nicecount" => "57 000",
             "count"     => "57000"
         ],
         [
-            "name"      => "#CommodoreClub",
             "url"       => "CommodoreClub",
-            "nicecount" => "64 000",
             "count"     => "64000"
         ],
         [
-            "name"      => "#MotorolaClub",
             "url"       => "MotorolaClub",
-            "nicecount" => "68 000",
             "count"     => "68000"
         ],
         [
-            "name"      => "#TromboneClub",
             "url"       => "TromboneClub",
-            "nicecount" => "76 000",
             "count"     => "76000"
         ],
         [
-            "name"      => "#WiFiClub",
             "url"       => "WiFiClub",
-            "nicecount" => "80 211",
             "count"     => "80211"
         ],
         [
-            "name"      => "#PajamaClub",
             "url"       => "PajamaClub",
-            "nicecount" => "90 000",
             "count"     => "90000"
         ],
         [
-            "name"      => "#TowerOfBabbleClub",
             "url"       => "TowerOfBabbleClub",
-            "nicecount" => "100 000",
             "count"     => "100000"
         ],
         [
-            "name"      => "#OrbitClub",
             "url"       => "OrbitClub",
-            "nicecount" => "110 000",
             "count"     => "110000"
         ],
         [
-            "name"      => "#MacClub",
             "url"       => "MacClub",
-            "nicecount" => "128 000",
             "count"     => "128000"
         ],
         [
-            "name"      => "#TwitterLeaverClub",
             "url"       => "TwitterLeaverClub",
-            "nicecount" => "140 000",
             "count"     => "140000"
         ],
         [
-            "name"      => "#GetALifeNoSrslyClub",
             "url"       => "GetALifeNoSrslyClub",
-            "nicecount" => "200 000",
             "count"     => "200000"
         ],
         [
-            "name"      => "#MeaninglessPostCountClub",
             "url"       => "MeaninglessPostCountClub",
-            "nicecount" => "231 568",
             "count"     => "231568"
         ],
         [
-            "name"      => "#ADNClub",
             "url"       => "ADNClub",
-            "nicecount" => "256 000",
             "count"     => "256000"
         ],
         [
-            "name"      => "#PensionersClub",
             "url"       => "PensionersClub",
-            "nicecount" => "401 000",
             "count"     => "401000"
         ],
         [
-            "name"      => "#LaughterClub",
             "url"       => "LaughterClub",
-            "nicecount" => "555 000",
             "count"     => "555000"
         ],
         [
-            "name"      => "#GatesClub",
             "url"       => "GatesClub",
-            "nicecount" => "640 000",
             "count"     => "640000"
         ],
         [
-            "name"      => "#JoyLuckClub",
             "url"       => "JoyLuckClub",
-            "nicecount" => "888 000",
             "count"     => "888000"
         ],
         [
-            "name"      => "#MillionairesClub",
             "url"       => "MillionairesClub",
-            "nicecount" => "1 000 000",
             "count"     => "1000000"
         ],
     ];
+
+    private $isInitialized = false;
 
     private $achievedClubs = [];
 
@@ -234,16 +164,24 @@ class PostClubs
 
     public function __construct(User $user)
     {
-        $this->init($this->user = $user);
+        $this->user = $user;
     }
 
     public function memberClubs()
     {
+        if (!$this->isInitialized) {
+            $this->init($this->user);
+        }
+
         return $this->achievedClubs;
     }
 
     public function nextClubs()
     {
+        if (!$this->isInitialized) {
+            $this->init($this->user);
+        }
+
         return $this->unachievedClubs;
     }
 
@@ -258,23 +196,21 @@ class PostClubs
                 $this->unachievedClubs[] = PostClub::wrap($club);
             }
         }
+
+        $this->isInitialized = true;
     }
 
     private function getPossibleClubs()
     {
-        $base = static::$clubs;
+        $clubs = static::$clubs;
 
         // whatever logic charl is using
-        $orphanBlack = PostClub::wrap([
-            "name"      => "#OrphanBlackClub",
-            "url"       => "OrphanBlackClub",
-            "nicecount" => 'TODO',
-            "count"     => 'TODO',
-        ]);
+        $clubs[] = [
+            "url"   => "OrphanBlackClub",
+            "count" => $this->user->id,
+        ];
 
-        // $clubs = $base + (USER_IS_SOMEHOW_ELIGIBLE ? $orphanBlack : null);
-        $clubs = $base + (false ? $orphanBlack : []);
 
-        return array_values($clubs);
+        return $clubs;
     }
 }
