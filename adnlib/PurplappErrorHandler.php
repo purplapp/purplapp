8bf066b01c46eaf718778e3e9621b51d82409860
Ciaran Downey <me@ciarand.me>
2014-10-04 11:15:41 -0700

    Remove all the old adnlib files
    Any functionality not currently in the Client can be added easily. No
    point in keeping this old nonsense around.

--- adnlib/PurplappErrorHandler.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	register_shutdown_function('shutdownFunction'); 
-    function shutDownFunction() { 
-        $error = error_get_last(); 
-        if ($error['type'] == 1) { 
-            echo "Critical PHP error! Unfortunately we can't continue, sorry.<br>Please refresh the page and try again, or return to the previous page.";    
-        } 
-    }	
-?>
