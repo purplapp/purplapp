87d9820d795e50fe89c61c1aab44a4ca3c0a4df8
Charlotte W <cwelkin@icloud.com>
2014-10-10 21:29:16 +0100

    Add in a necessary space in PostClub output

--- src/Adn/PostClub.php
+++ src/Adn/PostClub.php
@@ -7,7 +7,7 @@ class PostClub
     public function html()
     {
         return "<a href=\"https://alpha.app.net/hashtags/{$this->url}\">{$this->name()}</a>"
-            . "<i>({$this->niceCount()} posts)</i>";
+            . " <i>({$this->niceCount()} posts)</i>";
     }
 
     public function niceCount()
