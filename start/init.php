57dd157e86058804b68aecb0e18e1509656a6259
Charlotte W <cwelkin@icloud.com>
2014-10-11 15:45:06 +0100

    Start with 404 addition
    For pretty 404, see issue #54

--- start/init.php
+++ start/init.php
@@ -14,12 +14,20 @@ Dotenv::required([
     'GITHUB_URL'
 ]);
 
-$app["debug"] = true;
+$app["debug"] = false;
 
 $app->error(function (Exception $e, $code) use ($app) {
     if ($app["debug"]) {
         return;
     }
 
-    return new Response("uh oh");
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found. <br> Please refresh the page and try again, or return to the previous page.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong. <br> Unfortunately we cannot continue: so please refresh the page and try again, or return to the previous page.';
+    }
+
+    return new Response($message);
 });
