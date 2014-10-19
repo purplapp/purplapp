87fc6f573d5907b3432fb0b3c6f583975f9d464c
Charlotte W <cwelkin@icloud.com>
2014-10-19 18:25:39 +0100

    Add locale tests / fallback
    Closes #72

---
+++ tests/LocaleTest.php
@@ -0,0 +1,21 @@
+<?php namespace Purplapp\Tests;
+
+class LocaleTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_convert_correct_locale()
+    {
+        $locale = new \Purplapp\Adn\Locale("en_US");
+
+        $this->assertEquals("English (United States)", $locale);
+    }
+
+    public function it_should_error_correct_locale()
+    {
+        $locale = new \Purplapp\Adn\Locale("oh_MY");
+
+        $this->assertEquals("Unknown Locale", $locale);
+    }
+}
