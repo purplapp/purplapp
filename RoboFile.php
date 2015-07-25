11d4970212af82507515b9e3199f80b0cde0958a
Ciaran Downey <me@ciarand.me>
2015-07-25 02:58:27 -0700

    Try another thing

--- RoboFile.php
+++ RoboFile.php
@@ -44,22 +44,9 @@ class RoboFile extends TaskList
     {
         $this->stopOnFail(true);
 
-        $dir = sys_get_temp_dir();
-        $cache  = "{$dir}/tmp/cache";
-        $except = [
-            "{$cache}/assetic",
-            "{$cache}/twig",
-            "{$cache}/assetic/.gitignore",
-            "{$cache}/twig/.gitignore",
-        ];
-
-        $files = array_diff(
-            glob("./cache/{assetic/,twig/,}*", GLOB_BRACE),
-            $except
-        );
-
-        $this->taskDeleteDir($cache)->run();
-        $this->taskFileSystemStack()
+        $cache  = __DIR__ . "/tmp/cache";
+	$this->taskDeleteDir($cache)->run();
+	$this->taskFileSystemStack()
                 ->mkdir($cache)
                 ->mkdir("{$cache}/assetic")
                 ->mkdir("{$cache}/twig")
