e26e8be690ebf967d1794fa4ec27d39daa96322a
Ciaran Downey <me@ciarand.me>
2015-09-25 00:17:13 -0700

    Fix API changes in nice.social's API, move to HTTPS

--- src/Adn/NiceRankAwareClient.php
+++ src/Adn/NiceRankAwareClient.php
@@ -10,7 +10,7 @@ class NiceRankAwareClient extends Client
     public function getUserNiceRank(User $user, array $opts = [])
     {
         return $this->cache->get("user_nice_rank_{$user->id}", function () use ($user, $opts) {
-            $url = "http://api.nice.social/user/nicerank" . $this->buildQuery([
+            $url = "https://api.nice.social/user/nicerank" . $this->buildQuery([
                 "ids"          => $user->id,
                 "show_details" => true,
             ] + $opts);
